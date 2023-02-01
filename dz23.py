ff001b501974300c8a6e371fc9e7d082728aa76e
YunKaiAQA4957 <110104402+YunKaiAQA4957@users.noreply.github.com>
2023-02-01 23:38:44 +0700

    дополнение v1
    Добавлена ф-я get_info

--- dz23.py
+++ dz23.py
@@ -6,19 +6,33 @@ class Animal:
         self.age = age
         Animal.COUNT +=1
 
-dog = Animal('Yun_Kai', 'dog', 3)
-cat = Animal('Motya', 'cat', 11)
-bird = Animal('Kesha', 'bird', 5)
-horse = Animal('Plotva', 'horse', 6)
-animal_dict = Animal()
+    def get_info(self):
+        return f'{self.type_of_animal}:\n -Кличка животного: {self.name},\n -Возраст животного: {self.age}\n'
+
+dog = Animal('Юн-Кай/Yun-Kai', 'Собака/Dog', 3)
+cat = Animal('Мотя/Motya', 'Кошка/Cat', 11)
+bird = Animal('Кеша/Kesha', 'Птица/Bird', 5)
+horse = Animal('Плотва/Plotva', 'Лошадь/Horse', 6)
+# animal_dict = Animal()
 
 print(dog.__dict__)
 print(cat.__dict__)
 print(bird.__dict__)
 print(horse.__dict__)
-print(animal_dict.__dict__)
+# print(animal_dict.__dict__)
+
 print(Animal.__dict__)
+
+print(Animal.get_info(dog))
+print(Animal.get_info(cat))
+print(Animal.get_info(bird))
+print(Animal.get_info(horse))
+
 print(f'Количество животных {Animal.COUNT}')
+
+
+
+
 
 print('*'*40)
 input('Для выхода нажмите Enter')
